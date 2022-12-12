73fe245a08d05a7db581587628d8d3fa1b89bfdd
Kendell Andrews <kendellandrews2@gmail.com>
2022-12-12 12:31:50 -0700

    ?

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+      <img className='headerlogo' src='https://logos-world.net/amazon-logo/' />
+      <div className='headerSearch'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAcOZ0CY8fYSa-mpFWZnwb0Tduu4o0oi0w",
+    authDomain: "clone-cf650.firebaseapp.com",
+    projectId: "clone-cf650",
+    storageBucket: "clone-cf650.appspot.com",
+    messagingSenderId: "861003032718",
+    appId: "1:861003032718:web:18ece3f181a4752a2ec1df"
+  };
